a69d53463fee287b0b50499aa199c4cd500c5ab1
Igor Golovin <igoryator@gmail.com>
2018-10-02 23:47:11 +0300

    First step
    Get current coordinates and types in in status bar

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,83 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    geoSource = QGeoPositionInfoSource::createDefaultSource(this);
+
+    if(geoSource){
+
+        connect(geoSource,SIGNAL(error(QGeoPositionInfoSource::Error)),this,SLOT(geoError(QGeoPositionInfoSource::Error)));
+        connect(geoSource,SIGNAL(updateTimeout()),this,SLOT(geoTimeout()));
+        connect(geoSource,SIGNAL(positionUpdated(QGeoPositionInfo)),this,SLOT(geoUpdated(QGeoPositionInfo)));
+
+        updateTimer.setInterval(20000);
+        connect(&updateTimer,SIGNAL(timeout()),this,SLOT(requestUpdate()));
+        requestUpdate();
+        updateTimer.start();
+
+
+
+
+
+    } else {
+
+        ui->statusBar->showMessage("No coordinate source");
+
+    }
+
+
+    int z = 0;
+
+
 }
 
 MainWindow::~MainWindow()
 {
+
+    if(geoSource)
+        delete geoSource;
     delete ui;
+}
+
+
+
+void MainWindow::requestUpdate(){
+
+
+    geoSource->requestUpdate(10000);
+
+}
+
+void MainWindow::geoError(QGeoPositionInfoSource::Error positioningError){
+
+
+
+    ui->statusBar->showMessage("Error occured");
+
+
+}
+
+void MainWindow::geoTimeout(){
+
+
+    ui->statusBar->showMessage("Get coordinates timeout");
+
+}
+
+
+void MainWindow::geoUpdated(QGeoPositionInfo update){
+
+
+    QGeoCoordinate coordinate =  update.coordinate();
+
+    ui->statusBar->showMessage(coordinate.toString());
+
+
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLocation>
+#include <QtPositioning>
+#include <QTimer>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +18,18 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+public slots:
+
+
+    void geoUpdated(QGeoPositionInfo update);
+    void geoTimeout();
+    void geoError(QGeoPositionInfoSource::Error positioningError);
+    void requestUpdate();
+
 private:
     Ui::MainWindow *ui;
+    QGeoPositionInfoSource* geoSource = 0;
+    QTimer updateTimer;
 };
 
 #endif // MAINWINDOW_H
